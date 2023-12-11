40c4558c057ba1a65b2bcc9829915ec8df109ead
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2023-12-10 19:04:15 -0600

    Added Schema, seeds, and server
    Main schema tables have been added. Gave some base seeds for examples to work with, and set up base server functionality to connect the two. Need to add the actual server functions next

---
+++ seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES  
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Salesperson', 75000, 1),
+('Software Engineer', 90000, 2),
+('Account Manager', 100000, 3),
+('Lawyer', 120000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Smith', 1, NULL),
+('Evan', 'Hatley', 2, 1),
+('Jessica', 'Teegarden', 4, NULL),
+('Mac', 'Hatley', 3, NULL);
--- server.js
+++ server.js
@@ -1,2 +1,93 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql");
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3000;
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Mac+Ellie2023",
+    database: "employee_tracker_db"
+},
+console.log('Connected to Employee_Tracker database.')
+);
+
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'choices',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update an Employee Role'
+        ]
+    }
+];
+
+inquirer.prompt(questions)
+    .then((response) => {
+        switch (response.choice) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+
+            case 'Add a Department':
+                addDepartment();
+                break;
+
+            case 'Add a Role':
+                addRole();
+                break;
+
+            case 'Add an Employee':
+                addEmployee();
+                break;
+
+            case 'Update an Employee Role':
+                updateEmployeeRole();
+                break;
+        }
+    });
+
+function viewAllDepartments() {
+    db.query('SELECT * FROM department')
+
+}
+
+function viewAllRoles() {
+    db.query('SELECT * FROM role')
+}
+
+function viewAllEmployees() {
+    db.query('SELECT * FROM employee')
+}
+
+function addDepartment() {
+
+}
+
+function addRole() {
+
+}
+
+function addEmployee() {
+
+}
+
+function updateEmployeeRole() {
+
+
+}
